4550b5c6fd112dd38e53db7d9111615f771d8332
zulfuqarov <zulfuqarov.nebi@gmail.com>
2023-12-19 13:36:44 +0400

    tailwindcss configurations redy

--- screen/EditPage.js
+++ screen/EditPage.js
@@ -1,14 +1,14 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 
 const EditPage = () => {
   return (
     <View>
-      <Text>EditPage</Text>
+      <Text className="text-red-600">ilkin</Text>
     </View>
-  )
-}
+  );
+};
 
-export default EditPage
+export default EditPage;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
